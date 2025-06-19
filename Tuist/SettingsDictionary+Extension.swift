ed921e77f02193247c7b6832e5cb83c7cf842a52
Cesar Lima <cesarcamposdelima@gmail.com>
2025-06-19 19:59:14 -0300

    Add a SettingsDictionary extension to configure the default build settings to the project

---
+++ Tuist/SettingsDictionary+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsDictionary+Extension.swift
+//  Packages
+//
+//  Created by MacPro on 19/06/25.
+//
+
+import ProjectDescription
+
+extension SettingsDictionary {
+    static let commonBuildSettings: SettingsDictionary = [
+        "SWIFT_VERSION": "5.0",
+        "IPHONEOS_DEPLOYMENT_TARGET": "15.0",
+        "MACOSX_DEPLOYMENT_TARGET": "14.2"
+    ]
+}
