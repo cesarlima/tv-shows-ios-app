7acc73d385e5b1f5367d7157b1bbe4be5a8aa828
Cesar Lima <cesarcamposdelima@gmail.com>
2025-06-21 14:44:16 -0300

    Add URLSessionHttpClient. URLSessionHttpClient.init does not perform any request

--- Modules/Networking/NetworkingTests/URLSessionHttpClientTests.swift
+++ Modules/Networking/NetworkingTests/URLSessionHttpClientTests.swift
@@ -1,8 +1,89 @@
 import XCTest
+import Foundation
 @testable import Networking
 
+final class URLSessionHttpClient {
+    
+}
+
 final class URLSessionHttpClientTests: XCTestCase {
-    func test_example() {
-        XCTAssertTrue(true)
+    override func setUp() {
+        super.setUp()
+        URLProtocolStub.startInterceptingRequests()
     }
-} 
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_init_doesNotPerformAnyRequest() {
+        _ = URLSessionHttpClient()
+        
+        XCTAssertEqual(URLProtocolStub.requestsCount, 0)
+    }
+}
+
+private class URLProtocolStub: URLProtocol {
+    private static var stub: Stub?
+    private static var requestObserver: ((URLRequest) -> Void)?
+    private(set) static var requestsCount: Int = 0
+    
+    private struct Stub {
+        let data: Data?
+        let response: URLResponse?
+        let error: Error?
+    }
+    
+    static func stub(data: Data?, response: URLResponse?, error: Error?) {
+        stub = Stub(data: data, response: response, error: error)
+    }
+    
+    static func observeRequest(observer: @escaping (URLRequest) -> Void) {
+        requestObserver = observer
+    }
+    
+    static func startInterceptingRequests() {
+        URLProtocol.registerClass(URLProtocolStub.self)
+    }
+    
+    static func stopInterceptingRequests() {
+        URLProtocol.unregisterClass(URLProtocolStub.self)
+        stub = nil
+        requestObserver = nil
+        requestsCount = 0
+    }
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+    
+    override func startLoading() {
+        URLProtocolStub.requestsCount += 1
+        
+        if let requestObserver = URLProtocolStub.requestObserver {
+            client?.urlProtocolDidFinishLoading(self)
+            requestObserver(request)
+        }
+        
+        if let data = URLProtocolStub.stub?.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let response = URLProtocolStub.stub?.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        
+        if let error = URLProtocolStub.stub?.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        
+        client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {}
+}
