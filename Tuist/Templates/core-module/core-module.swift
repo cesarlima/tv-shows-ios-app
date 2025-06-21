4c8dd35f7d117d4919d7e8ad2c8b52202e4f672e
Cesar Lima <cesarcamposdelima@gmail.com>
2025-06-20 21:51:22 -0300

    Refactor core-module template to use the new helpers to create the core modules.

--- Tuist/Templates/core-module/core-module.swift
+++ Tuist/Templates/core-module/core-module.swift
@@ -7,102 +7,48 @@
 
 import ProjectDescription
 
-let nameAttribute: Template.Attribute = .required("name")
-let bundleIdAttribute: Template.Attribute = .required("bundleId")
+fileprivate let moduleName: Template.Attribute = .required("name")
 
 let template = Template(
     description: "A template for core modules following TMA architecture",
     attributes: [
-        nameAttribute,
-        bundleIdAttribute,
+        moduleName,
         .optional("platform", default: "ios"),
     ],
     items: [
         .string(
-            path: "Modules/\(nameAttribute)/Project.swift",
+            path: "Modules/\(moduleName)/Project.swift",
             contents: """
             import ProjectDescription
+            import ProjectDescriptionHelpers
 
-            let project = Project(
-                name: "\(nameAttribute)",
-                organizationName: "TVShowsApp",
-                options: .options(
-                    automaticSchemesOptions: .disabled,
-                    disableBundleAccessors: false
-                ),
-                packages: [],
-                settings: .settings(
-                    base: [
-                        "SWIFT_VERSION": "5.0",
-                        "IPHONEOS_DEPLOYMENT_TARGET": "15.0"
-                    ]
-                ),
-                targets: [
-                    // Source target
-                    .target(name: "\(nameAttribute)",
-                            destinations: [.mac, .iPhone],
-                            product: .framework,
-                            bundleId: "\(bundleIdAttribute)",
-                            deploymentTargets: .multiplatform(macOS: "14.2"),
-                            infoPlist: .default,
-                            sources: ["Sources/**"],
-                            dependencies: [.target(name: "\(nameAttribute)Interface")]),
-                    
-                    // Interface target
-                    .target(name: "\(nameAttribute)Interface",
-                            destinations: [.mac, .iPhone],
-                            product: .framework,
-                            bundleId: "\(bundleIdAttribute).interface",
-                            deploymentTargets: .multiplatform(macOS: "14.2"),
-                            infoPlist: .default,
-                            sources: ["Interface/**"]
-                           ),
-                    
-                    // Tests target
-                    .target(name: "\(nameAttribute)Tests",
-                            destinations: [.mac, .iPhone],
-                            product: .unitTests,
-                            bundleId: "\(bundleIdAttribute).tests",
-                            deploymentTargets: .multiplatform(macOS: "14.2"),
-                            infoPlist: .default,
-                            sources: ["Tests/**"],
-                            dependencies: [
-                                .target(name: "\(nameAttribute)"),
-                                .target(name: "\(nameAttribute)Testing")
-                            ]
-                           ),
-                    
-                    // Testing target
-                    .target(name: "\(nameAttribute)Testing",
-                            destinations: [.mac, .iPhone],
-                            product: .framework,
-                            bundleId: "\(bundleIdAttribute).testing",
-                            deploymentTargets: .multiplatform(macOS: "14.2"),
-                            infoPlist: .default,
-                            sources: ["Testing/**"],
-                            dependencies: [
-                                .target(name: "\(nameAttribute)Interface")
-                            ]
-                           )
+            let configuration = CoreModuleConfiguration(
+                name: "\(moduleName)"
+            )
+
+            let project = Project.tmaModule(
+                configuration: configuration,
+                additionalDependencies: [
+                    // Add module-specific dependencies here
                 ]
             )
             """
         ),
-        .directory(
-            path: "Modules/\(nameAttribute)/Sources",
-            sourcePath: "Sources"
+        .file(
+            path: "Modules/\(moduleName)/\(moduleName)/Module.swift",
+            templatePath: "Sources/Module.swift"
         ),
-        .directory(
-            path: "Modules/\(nameAttribute)/Interface",
-            sourcePath: "Interface"
+        .file(
+            path: "Modules/\(moduleName)/\(moduleName)Interface/ModuleInterface.swift",
+            templatePath: "Interface/ModuleInterface.swift"
         ),
-        .directory(
-            path: "Modules/\(nameAttribute)/Tests",
-            sourcePath: "Tests"
+        .file(
+            path: "Modules/\(moduleName)/\(moduleName)Tests/ModuleTests.swift",
+            templatePath: "Tests/ModuleTests.swift"
         ),
-        .directory(
-            path: "Modules/\(nameAttribute)/Testing",
-            sourcePath: "Testing"
+        .file(
+            path: "Modules/\(moduleName)/\(moduleName)Testing/ModuleTesting.swift",
+            templatePath: "Testing/ModuleTesting.swift"
         )
     ]
 )
